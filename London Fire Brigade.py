95824b4765e1cff50a7902d8a620a585895045c6
Xue Kai (FCA) <t8668kx@cag.cdroot.net>
2026-04-08 23:58:14 -0400

    copied London Fire Brigade streamlit app

---
+++ London Fire Brigade.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+st.markdown("""
+<h1 style='text-align: center;'>London Fire Brigade</h1>
+<h3 style='text-align: center;'>- Attendance Time Prediction</h3>
+<br><br><br><br><br><br><br><br><br><br>
+<h5 style='text-align: center;'>Waldemar Schock, Kai Xue</h5>
+<h5 style='text-align: center;'>April 2026</h5>
+""", unsafe_allow_html=True)
